645c8840d45da7ecaf2335d1d8ba4e3cb3d5bac1
keisuke kamiya <luckey.star123@gmail.com>
2013-10-20 20:29:02 +0900

    NGワードの実�
    NgWordからNGワードを取ってきて照合。ダメなものは表示を行い、投稿させないようにする。

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,24 +1,33 @@
 <?php
 
 class PostsController extends AppController{
-	public $helpers = array('Html','Form');
-	public $components = array('Session');
+        public $helpers = array('Html','Form');
+        public $components = array('Session');
+        public $uses = array('Post','NgWord');
 
+        public function index(){
+                $this->set('posts', $this->Post->find('all'));
+                $this->set('ng',$this->NgWord->find('all'));
+        }
 
-	public function index(){
-		$this->set('posts', $this->Post->find('all'));
-	}
-
-	public function add(){
-		if($this->request->is('post')){
-			$this->Post->create();
-			if($this->Post->save($this->request->data)){
-				$this->Session->setFlash(__('Success!'));
-				return $this->redirect(array('action'=>'index'));
-			}
-			$this->Session->setFlash(__('Failed'));
-		}
-	}
+        public function add(){
+                if($this->request->is('post')){
+                        $this->Post->create();
+                        $array = $this->NgWord->find('all');
+                        foreach ($array as $value) {
+                                if( !strpos($this->request->data['Post']['tweet'],$value['NgWord']['word']) ){
+                                        $this->Session->setFlash(__($value['NgWord']['word'].' is NGword'));
+                                        return $this->redirect(array('action'=>'index'));
+                                }
+                        }
+                        if($this->Post->save($this->request->data)){
+                                $this->Session->setFlash(__('Success!'));
+                                return $this->redirect(array('action'=>'index'));
+                        }
+                        $this->Session->setFlash(__('Failed'));
+                        return $this->redirect(array('action'=>'index'));
+                }
+        }
 
 }
 
