a23db104a2bef622c5387a7e71a0360815af1925
keisuke kamiya <luckey.star123@gmail.com>
2013-10-20 20:02:02 +0900

    Scaffoldの何かを追加

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -4,6 +4,7 @@ class PostsController extends AppController{
 	public $helpers = array('Html','Form');
 	public $components = array('Session');
 
+
 	public function index(){
 		$this->set('posts', $this->Post->find('all'));
 	}
